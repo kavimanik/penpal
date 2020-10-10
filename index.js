8ee4b5bf17321c190c5425e98605cbd4b17d5d79
tfich <tfichiera@gmail.com>
2020-10-10 11:45:32 -0700

    folder structure

---
+++ index.js
@@ -0,0 +1,23 @@
+import cookieParser from 'cookie-parser'
+import 'dotenv/config.js'
+import express from 'express'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+import apiRouter from './routes/api'
+
+const app = express()
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(join(__dirname, 'public')))
+
+app.get('/', (_, res) => res.sendFile(join(__dirname, 'views', 'index.html')))
+
+app.use('/api', apiRouter)
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`)
+})
