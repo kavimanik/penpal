974b18aff396f6a54d2a85a7910af5041f54f3f9
tfich <tfichiera@gmail.com>
2020-10-10 16:24:20 -0700

    merge

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const cookieParser = require('cookie-parser')
 const express = require('express')
-const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars')
 const { join } = require('path')
 const apiRouter = require('./routes/api')
 const mongoose = require('mongoose')
@@ -17,25 +17,23 @@ mongoose.connect(process.env.MONGO_URI, {
 const app = express()
 
 const hbs = exphbs.create({
-    extname: '.hbs'
-});
+  extname: '.hbs'
+})
 
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(join(__dirname, 'public')))
 
-app.engine('.hbs', hbs.engine);
-app.set('view engine', '.hbs');
-app.set('views', join(__dirname, 'views'));
+app.engine('.hbs', hbs.engine)
+app.set('view engine', '.hbs')
+app.set('views', join(__dirname, 'views'))
 
-
-app.get('/', (req, res) => res.render('index.hbs'));
-app.get('/dashboard', (req, res) => res.render('dashboard.hbs'));
-app.get('/signup', (req, res) => res.render('signup.hbs'));
-app.get('/writer', (req, res) => res.render('writer.hbs'));
-app.get('/login', (req, res) => res.render('login.hbs'));
-
+app.get('/', (req, res) => res.render('index.hbs'))
+app.get('/login', (req, res) => res.render('login.hbs'))
+app.get('/signup', (req, res) => res.render('signup.hbs'))
+app.get('/writer', (req, res) => res.render('writer.hbs'))
+app.get('/dashboard', (req, res) => res.render('dashboard.hbs'))
 
 app.use('/api', apiRouter)
 
