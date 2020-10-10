def07a268d93b13c250cb526e4f280a6ac268941
Nicholas Paulette <nicholasapaulette@gmail.com>
2020-10-10 16:18:32 -0700

    hbs conversion in progress

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cookieParser = require('cookie-parser')
 const express = require('express')
+const exphbs = require('express-handlebars');
 const { join } = require('path')
 const apiRouter = require('./routes/api')
 
@@ -7,16 +8,26 @@ require('dotenv/config')
 
 const app = express()
 
+const hbs = exphbs.create({
+    extname: '.hbs'
+});
+
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(join(__dirname, 'public')))
 
-app.get('/', (_, res) => res.sendFile(join(__dirname, 'views', 'index.html')))
-app.get('/dashboard', (_, res) => res.sendFile(join(__dirname, 'views', 'dashboard.html')))
-app.get('/signup', (_, res) => res.sendFile(join(__dirname, 'views', 'signup.html')))
-app.get('/writer', (_, res) => res.sendFile(join(__dirname, 'views', 'writer.html')))
-app.get('/login', (_, res) => res.sendFile(join(__dirname, 'views', 'login.html')))
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
+app.set('views', join(__dirname, 'views'));
+
+
+app.get('/', (req, res) => res.render('index.hbs'));
+app.get('/dashboard', (req, res) => res.render('dashboard.hbs'));
+app.get('/signup', (req, res) => res.render('signup.hbs'));
+app.get('/writer', (req, res) => res.render('writer.hbs'));
+app.get('/login', (req, res) => res.render('login.hbs'));
+
 
 app.use('/api.js', apiRouter)
 
