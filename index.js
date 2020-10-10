6b2580a7bc14e387e4a069042a5603b958834568
Nicholas Paulette <nicholasapaulette@gmail.com>
2020-10-10 15:02:16 -0700

    created skeleton of templates

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.static(join(__dirname, 'public')))
 
 app.get('/', (_, res) => res.sendFile(join(__dirname, 'views', 'index.html')))
-app.get('/letterDashboardTemplate', (_, res) => res.sendFile(join(__dirname, 'views', 'letterDashboardTemplate.html')))
+app.get('/dashboard', (_, res) => res.sendFile(join(__dirname, 'views', 'dashboard.html')))
+app.get('/signup', (_, res) => res.sendFile(join(__dirname, 'views', 'signup.html')))
+app.get('/writer', (_, res) => res.sendFile(join(__dirname, 'views', 'writer.html')))
+app.get('/login', (_, res) => res.sendFile(join(__dirname, 'views', 'login.html')))
 
 app.use('/api.js', apiRouter)
 
